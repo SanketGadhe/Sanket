6de2b4a17b6fdfaf6a9c42ca00fbb9fc12444d87
SANKET VILAS GADHE <98447853+SanketGadhe@users.noreply.github.com>
2023-06-01 23:46:14 +0530

    Create Day6_compare.cpp

---
+++ Day6_compare.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter Two numbers:";
+    cin>>a>>b;s
+    cout<<"1 For True 0 For False";
+    cout<<"\nIs a<b: "<< (a<b);
+    cout<<"\nIS a>b: "<< (a>b);
+    cout<<"\nIS a==b: "<< (a==b);
+    return 0;
+}
